56390d0ec88688910b3898aa8e44a4cf58d01abd
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-08-01 11:12:35 +0200

    now using service a spayload

--- src/Command/NewVirtualHostEventCommand.php
+++ src/Command/NewVirtualHostEventCommand.php
@@ -5,6 +5,7 @@ namespace TheAentMachine\AentTraefik\Command;
 use TheAentMachine\Aenthill\CommonEvents;
 use TheAentMachine\Command\AbstractJsonEventCommand;
 use TheAentMachine\Question\CommonValidators;
+use TheAentMachine\Service\Exception\ServiceException;
 use TheAentMachine\Service\Service;
 
 class NewVirtualHostEventCommand extends AbstractJsonEventCommand
@@ -15,26 +16,28 @@ class NewVirtualHostEventCommand extends AbstractJsonEventCommand
     }
 
     /**
-     * @param array $payload
+     * @param mixed[] $payload
      * @return array|null
-     * @throws \TheAentMachine\Service\Exception\ServiceException
+     * @throws ServiceException
      */
     protected function executeJsonEvent(array $payload): ?array
     {
-        $serviceName = $payload['service'];
-        $virtualHost = $payload['virtualHost'] ?? null;
-        $virtualPort = $payload['virtualPort'];
+        $service = Service::parsePayload($payload);
+        $serviceName = $service->getServiceName();
+        $ports = $service->getPorts();
 
-        $service = new Service();
-
-        if ($virtualHost === null) {
-            $this->output->writeln("You are about to <info>configure the domain name</info> of the service <info>$serviceName</info> in the reverse proxy (Traefik).");
-
-            $virtualHost = $this->getAentHelper()->question('What is the domain name of this service?')
-                ->compulsory()
-                ->setValidator(CommonValidators::getDomainNameValidator())
-                ->ask();
+        if (!empty($ports) && isset($ports[0])) {
+            $virtualPort = $ports[0];
+        } else {
+            $virtualPort = "80";
         }
+
+        $this->output->writeln("You are about to <info>configure the domain name</info> of the service <info>$serviceName</info> in the reverse proxy (Traefik).");
+
+        $virtualHost = $this->getAentHelper()->question('What is the domain name of this service?')
+            ->compulsory()
+            ->setValidator(CommonValidators::getDomainNameValidator())
+            ->ask();
 
         $this->output->writeln("<info>Adding host redirection from '$virtualHost' to service '$serviceName' on port '$virtualPort'</info>");
 
@@ -42,7 +45,7 @@ class NewVirtualHostEventCommand extends AbstractJsonEventCommand
         $service->addLabel('traefik.enable', 'true');
         $service->addLabel('traefik.backend', $serviceName);
         $service->addLabel('traefik.frontend.rule', 'Host:' . $virtualHost);
-        $service->addLabel('traefik.port', (string)$virtualPort);
+        $service->addLabel('traefik.port', $virtualPort);
 
         return $service->jsonSerialize();
     }
